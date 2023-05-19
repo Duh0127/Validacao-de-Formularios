fdddf267dd9274a4006a531b5a331b9f91e9c55e
Duh0127 <rodatupmocgamer22@gmail.com>
2023-05-19 16:59:50 -0300

    [4] - Aula7: LocalStorage

--- js/script.js
+++ js/script.js
@@ -3,6 +3,23 @@ import ehMaiorDeIdade from "./valida-idade.js";
 const camposDoFormulario = document.querySelectorAll('[required]')
 
 
+const formulario = document.querySelector("[data-formulario]");
+formulario.addEventListener("submit", (e)=> {
+    e.preventDefault();
+
+    const listaRespostas = {
+        "nome": e.target.elements["nome"].value,
+        "email": e.target.elements["email"].value,
+        "rg": e.target.elements["rg"].value,
+        "cpf": e.target.elements["cpf"].value,
+        "aniversario": e.target.elements["aniversario"].value
+    }
+    localStorage.setItem("cadastro", JSON.stringify(listaRespostas));
+    window.location.href = './abrir-conta-form-2.html';
+})
+
+
+
 camposDoFormulario.forEach((campo) => {
     campo.addEventListener("blur", () => verificaCampo(campo));
     campo.addEventListener("invalid", evento => evento.preventDefault());
